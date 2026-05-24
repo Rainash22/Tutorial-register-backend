2bf66cfb0776cc801d6b2822ccc9755eae537e04
Rain Ash <asheemeec@gmail.com>
2026-05-24 11:28:35 +0530

    acube site updated

--- src/main/java/com/tutorialregister/config/SecurityConfig.java
+++ src/main/java/com/tutorialregister/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "https://tut-pro-frontend.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "https://acubeweb.in", "https://www.acubeweb.in"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
